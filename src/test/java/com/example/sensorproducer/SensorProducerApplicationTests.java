d8df2329b0cd7a5932c0c255512c29e50451847b
prahulk46a <prahulk212@gmail.com>
2025-09-17 22:58:06 +0530

    first commit

---
+++ src/test/java/com/example/sensorproducer/SensorProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sensorproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
